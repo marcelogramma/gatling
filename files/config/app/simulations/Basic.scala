fcf8fe570c49da6f7fc24ce6ab929f25f9378b18
Ezequiel <ezequielszurita@gmail.com>
2023-01-25 17:12:04 -0300

    Se agregan prueba de login

--- files/config/app/simulations/Basic.scala
+++ files/config/app/simulations/Basic.scala
@@ -2,11 +2,11 @@ import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import scala.concurrent.duration._
 
-class inyectarUsuariosInmediatamente extends Simulation {
+class inyectarUsuariosConcurrentemente extends Simulation {
         val httpProtocol = http
         .baseUrl("wwwurl")
         val scn = scenario("scenarioName")
-        .exec(http("request_ok").get("getVar"))
+        .exec(http("request_ok").get("getVar").check(status.is(httpcodelogin)))
 // inyectar X usuarios inmediatamente
         setUp(scn.inject(atOnceUsers(requestcount))).protocols(protocolvariable)
         }
@@ -15,7 +15,35 @@ class inyectarUsuariosDistribuidosEnMinutos extends Simulation {
         val httpProtocol = http
         .baseUrl("wwwurl")
         val scn = scenario("scenarioName")
-        .exec(http("request_ok").get("getVar"))
+        .exec(http("request_ok").get("getVar").check(status.is(httpcodelogin)))
 // inyectar X usuarios en un periodo de X minutos de manera distribuida
        setUp(scn.inject(rampUsers(requestcount) during (timevariable minutes))).protocols(protocolvariable)
         }
+
+class testLoginUsuariosDistribuidosEnMinutos extends Simulation {
+
+    val httpProtocol = http
+        .baseUrl("wwwurl")
+        .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+        .acceptLanguageHeader("en-US,en;q=0.5")
+        .acceptEncodingHeader("gzip, deflate")
+        .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
+    val userCount =requestcount
+    val rampUpTime =tiempologin seconds
+    val testDuration =maxiempologin minutes
+
+    val loginScenario = scenario("Login")
+        .exec(http("login_request")
+            .post("getVar")
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .formParam("usernamelogin", "secretusernamelogin")
+            .formParam("passwordlogin", "secretpasswordlogin!")
+            .check(status.is(httpcodelogin))
+            .check(regex("""(?i)ERROR: msjerrorlogin""").count.is(0))
+        )
+
+    setUp(
+        loginScenario.inject(atOnceUsers(userCount))
+    ).protocols(httpProtocol)
+    .maxDuration(testDuration)
+}
--- files/config/scripts/gatlingconfig.sh
+++ files/config/scripts/gatlingconfig.sh
@@ -7,7 +7,14 @@ sed -i "s/scenarioName/$scenarioName/g" /home/ec2-user/simulations/Basic.scala
 sed -i "s/protocolvariable/$protocolvariable/g" /home/ec2-user/simulations/Basic.scala
 sed -i "s#getVar#$getvar#g" /home/ec2-user/simulations/Basic.scala
 sed -i "s/timevariable/$timevariable/g" /home/ec2-user/simulations/Basic.scala
-
+sed -i "s/tiempologin/$tiempologin/g" /home/ec2-user/simulations/Basic.scala
+sed -i "s/maxiempologin/$maxiempologin/g" /home/ec2-user/simulations/Basic.scala
+sed -i "s#secretusernamelogin#$secretusernamelogin#g" /home/ec2-user/simulations/Basic.scala
+sed -i "s#usernamelogin#$usernamelogin#g" /home/ec2-user/simulations/Basic.scala
+sed -i "s#secretpasswordlogin#$secretpasswordlogin#g" /home/ec2-user/simulations/Basic.scala
+sed -i "s#passwordlogin#$passwordlogin#g" /home/ec2-user/simulations/Basic.scala
+sed -i "s#httpcodelogin#$httpcodelogin#g" /home/ec2-user/simulations/Basic.scala
+sed -i "s#msjerrorlogin#$msjerrorlogin#g" /home/ec2-user/simulations/Basic.scala
 
 sudo timedatectl set-timezone $timezone
 sudo systemctl reload httpd
