63b5323fe3ef4de187a691612901069c6428cde0
marcelogramma <marcelogrammatico02@gmail.com>
2023-02-11 17:31:04 -0300

    Add scenario RampUsers

--- files/config/app/simulations/MyCustomsSimulation.scala
+++ files/config/app/simulations/MyCustomsSimulation.scala
@@ -26,3 +26,44 @@ class GetGlobalSimulation extends Simulation {
       global.successfulRequests.percent.gt(95)
     )
 }
+class RampUsersLoadSimulation extends Simulation {
+
+  //1. HTTP Configuration
+  val httpConf = (PerfTestConfig.baseUrl)
+  val httpProtocol: HttpProtocolBuilder = http
+    .baseUrl(httpConf)
+    .header("Accept", "application/json")
+
+  //2. Call Definitions
+  def getAllComments: ChainBuilder = {
+      exec(
+        http("Get All Clothes")
+          .get("/ropa")
+          .check(status.in(200 to 304))
+      )
+  }
+
+  def getAllPosts: ChainBuilder = {
+      exec(
+        http("Get All Travels")
+          .get("/viajes")
+          .check(status.in(200 to 304))
+      )
+  }
+
+  //3. Scenario Definition
+  val scn: ScenarioBuilder = scenario("Eight Scenario")
+    .exec(getAllComments)
+    .pause(5)
+    .exec(getAllPosts)
+    .pause(5)
+    .exec(getAllComments)
+
+  //4. Load Scenario
+  setUp(
+    scn.inject(
+      nothingFor(5 seconds),
+      constantUsersPerSec(10) during (10 seconds),
+      rampUsersPerSec(1) to (5) during (20 seconds))
+  ).protocols(httpProtocol)
+}
